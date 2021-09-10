c6f44f516dc0eeecec231a2d7a2c8388a07c909d
David A <davidaster007@gmail.com>
2021-09-10 08:27:27 +0200

    Added databinding, added some resources(colors,dimensions,strings,styles)

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -1,28 +1,28 @@
 package com.example.trivia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
-import android.util.Log;
 
-import com.example.trivia.data.AnswerListAsyncResponse;
 import com.example.trivia.data.Repository;
-import com.example.trivia.model.Question;
+import com.example.trivia.databinding.ActivityMainBinding;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private int currentQuestionIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Repository().getQuestions(new AnswerListAsyncResponse() {
-            @Override
-            public void processFinished(ArrayList<Question> questionArrayList) {
-                Log.d("Main", "onCreate(): " + questionArrayList);
-            }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        new Repository().getQuestions(questionArrayList -> {
+            binding.questionTextview.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
         });
     }
 }
