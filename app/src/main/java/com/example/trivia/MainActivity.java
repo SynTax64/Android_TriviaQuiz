4e68ea6d0bd75ad82d93540c32f9655098942644
David A <davidaster007@gmail.com>
2021-09-10 09:25:03 +0200

    Added button listeners, checking a correct answer, updating the question counter

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.trivia;
 
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
-import android.os.Bundle;
-
 import com.example.trivia.data.Repository;
 import com.example.trivia.databinding.ActivityMainBinding;
+import com.example.trivia.model.Question;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private int currentQuestionIndex = 0;
+    List<Question> questions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +28,48 @@ public class MainActivity extends AppCompatActivity {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        new Repository().getQuestions(questionArrayList -> {
-            binding.questionTextview.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
+        questions = new Repository().getQuestions(questionArrayList -> {
+            binding.questionTextview.setText(questionArrayList.get(currentQuestionIndex)
+                    .getAnswer());
+            updateCounter(questionArrayList);
         });
+
+        binding.buttonNext.setOnClickListener(view -> {
+            currentQuestionIndex = (currentQuestionIndex + 1) % questions.size();
+            updateQuestion();
+        });
+
+        binding.buttonTrue.setOnClickListener(view -> {
+            checkAnswer(true);
+            updateQuestion();
+        });
+
+        binding.buttonFalse.setOnClickListener(view -> {
+            checkAnswer(false);
+            updateQuestion();
+        });
+    }
+
+    private void checkAnswer(boolean userChoseCorrect) {
+        boolean answer = questions.get(currentQuestionIndex).isAnswerTrue();
+        int snackMessageId = 0;
+        if (userChoseCorrect == answer) {
+            snackMessageId = R.string.correct_answer;
+        } else {
+            snackMessageId = R.string.incorrect_answer;
+        }
+
+        Snackbar.make(binding.cardView, snackMessageId, Snackbar.LENGTH_SHORT).show();
+
+    }
+
+    private void updateCounter(@NonNull ArrayList<Question> questions) {
+        binding.textViewOutOf.setText(String.format(getString(R.string.text_formatted), currentQuestionIndex, questions.size()));
+    }
+
+    private void updateQuestion() {
+        String question = questions.get(currentQuestionIndex).getAnswer();
+        binding.questionTextview.setText(question);
+        updateCounter((ArrayList<Question>) questions);
     }
 }
